326e0505981679e73ce0544972aa7bad85eb7ef6
Jérémy <jeremy.rolland@le-campus-numerique.fr>
2025-01-07 15:04:00 +0100

    resolution exo-5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,11 @@
-// you can write js here
+let input = 'Turpentine and turtles';   //phrase d'entrée
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];  //tableau des voyelles
+let resultArray = [];   //Liste vide
 
-console.log('exo-5');
+//Parcours la phrase d'entrée pour trouver des voyelles
+for (let i=0; i<input.length; i++) {
+   if(vowels.includes(input[i])) {
+    resultArray.push(input[i]);
+   }
+}
+console.log(resultArray.join('').toUpperCase());
