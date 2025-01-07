ca3642c361941fff64fae219bcf77744167c3461
Jérémy <jeremy.rolland@le-campus-numerique.fr>
2025-01-07 13:49:47 +0100

    en fait c'etait l'exo-2 avant et la c'est le vrai exo-3

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,40 @@
-// you can write js here
-console.log('exo-3');
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?"); //Demande une entrée à l'utilisateur
+
+playGame(); //Fonction principale
+
+// Vérifie que le choix utilisateur correspond aux critères
+function getPlayerChoice(playerInput) {
+    const temp = playerInput.toLowerCase();
+    if(temp === 'rock' || temp === 'paper' || temp === 'scissors') {
+        return temp;
+    } else {
+        console.log("Erreur de saisie'")
+    }
+}
+
+//Génère un jet pour l'ordinateur
+function getComputerChoice() {
+    const possibility = ["rock", "paper", "scissors"];
+    return possibility[Math.floor(Math.random() * 3)];
+}
+
+//Gère le résultat du match
+function findWinner(getPlayerChoice, getComputerChoice) {
+   const outcomes = {
+    rock: { rock: "Tied", scissors: "Won", paper: "Lost"},
+    paper: { paper: "Tied", rock: "Won", scissors: "Lost" },
+    scissors: { scissors: "Tied", paper: "Won", rock: "Lost"},
+   };
+   return outcomes[getPlayerChoice][getComputerChoice];
+}
+
+//fonction principale, appele les fonctiosn nécéssaires au déroulement d'une partie
+function playGame() {
+    const uChoice = getPlayerChoice(playerInput);
+    const computerChoice = getComputerChoice();
+
+    console.log("user:" ,uChoice);
+    console.log("computer", computerChoice);
+
+    console.log(findWinner(uChoice, computerChoice));
+}
