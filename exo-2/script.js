26a60e4fd5a5366d45fcfe3d821a39d53a9d2ff0
Jérémy <jeremy.rolland@le-campus-numerique.fr>
2025-01-07 11:44:50 +0100

    résolution exo-3

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,27 @@
-// you can write js here
-console.log('exo-2');
+const myDate = new Date();  //Déclaration objet Date()
+const messageDayWeek = 'Jour semaine'   //déclaration message semaine
+const messageDayWeekEnd = 'Jour week-end'   //declaration message week-end
+let actualDay;  //déclaration variable pour le jour
+let actualHour; //declaration variable pour l'heure
+
+const isTesting = true; //déclaration variable mode test
+//Assignation des variables de test si condition vérifiée
+if (isTesting) {    
+    actualDay = Number(prompt("Quel jour utiliser ? (Sunday - Saturday : 0 - 6)")); //on demande une valeur à l'utilisateur et on cast en nombre
+    actualHour =Number(prompt("Quelle heure utiliser ?"));  //on demande une valeur à l'utilisateur et on cast en nombre
+} else {
+    actualDay = myDate.getDay();    //On récupère le jour actuel
+    actualHour = myDate.getHours(); //On récupère l'heure actuelle
+}
+//Vérifie si on est samedi ou dimanche
+if(actualDay === 0 || actualDay === 6) {
+    console.log(messageDayWeekEnd);
+} else {
+    console.log(messageDayWeek);
+}
+//Vérifie si on est samedi ou dimanche ou vendredi à partir de 17h
+if(actualDay === 0 || actualDay === 6 || (actualDay === 5 && actualHour >= 17)) {
+    console.log(messageDayWeekEnd);
+} else {
+    console.log(messageDayWeek);
+}
