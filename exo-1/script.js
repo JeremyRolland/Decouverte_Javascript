519d6513c44f0d21d1cb09a76693b5443299f138
Jérémy ROLLAND <jeremy.rolland@Digital-Grenoble.local>
2025-01-07 10:59:34 +0100

    Ajout commentaire ligne 3

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,5 +1,5 @@
 const kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
-const celsius = kelvins - 273;   //Déclaration variable celsius résultat de la variable kelvin à laquelle on soustrait 273
-let fahrenheits = celsius * (9/5) + 32;
+const celsius = kelvins - 273;   //Déclaration constante celsius résultat de la variable kelvin à laquelle on soustrait 273
+let fahrenheits = celsius * (9/5) + 32; //Déclaration variable fahrenheits qui va changer de valeur donc avec let
 fahrenheits = Math.floor(fahrenheits); //Utilisation de la fonction floor de la librairie Math afin d'arrondir la valeur de fahrenheits
 console.log(fahrenheits);   //Affichage de la variable fahrenheits dans la console
