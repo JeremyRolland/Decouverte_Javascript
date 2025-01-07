b9eb55b282ae8f2fa849b70d26843d65019b129c
Jérémy ROLLAND <jeremy.rolland@Digital-Grenoble.local>
2025-01-07 10:57:28 +0100

    Résolution exo-1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,5 @@
-// you can write js here
-console.log('hello from file');
+const kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+const celsius = kelvins - 273;   //Déclaration variable celsius résultat de la variable kelvin à laquelle on soustrait 273
+let fahrenheits = celsius * (9/5) + 32;
+fahrenheits = Math.floor(fahrenheits); //Utilisation de la fonction floor de la librairie Math afin d'arrondir la valeur de fahrenheits
+console.log(fahrenheits);   //Affichage de la variable fahrenheits dans la console
